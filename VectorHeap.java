c8409ed7e0ad8dbdf8f798aab525aaedc07458ac
MENÉNDEZ GUERRA <67031016+MENENDEZGUERRA@users.noreply.github.com>
2023-04-03 17:59:21 -0600

    ActualizaciónVectorHeap con Documentación Javadod

--- VectorHeap.java
+++ VectorHeap.java
@@ -1,46 +1,59 @@
 import java.util.Vector;
-
+/**
+ * La clase VectorHeap implementa la interfaz Priority, proporcionando un montículo
+ * mínimo respaldado por un Vector. La clase acepta elementos genéricos que deben
+ * implementar la interfaz Comparable para permitir la comparación y ordenación de
+ * los elementos en el montículo.
+ *
+ * @param <E> el tipo genérico de los elementos que se van a almacenar en el montículo.
+ */
 public class VectorHeap<E extends Comparable<E>> implements Priority<E> {
 
-    protected Vector<E> data; // the data, kept in heap order
+    protected Vector<E> data; // el Vector que mantiene los datos en orden de montículo
 
     /**
-     *
+     * Construye un montículo vacío.
      */
     public VectorHeap() {
         data = new Vector<>();
     }
 
     /**
+     * Construye un montículo a partir de un vector existente, agregando todos los elementos
+     * del vector al montículo.
      *
-     * @param v
+     * @param v el vector que contiene los elementos a agregar al montículo.
      */
     public VectorHeap(Vector<E> v) {
-        data = new Vector<>(v.size()); // we know ultimate size
-        for (int i = 0; i < v.size(); i++) {// add elements to heap
-            add(v.get(i));
+        data = new Vector<>(v.size()); // conocemos el tamaño final
+        for (int i = 0; i < v.size(); i++) {
+            add(v.get(i)); // agrega los elementos al montículo
         }
     }
 
     /**
+     * Construye un montículo a partir de un arreglo existente, agregando todos los elementos
+     * del arreglo al montículo.
      *
-     * @param arr
+     * @param arr el arreglo que contiene los elementos a agregar al montículo.
      */
     public VectorHeap(E[] arr) {
-        data = new Vector<>(arr.length); // we know ultimate size
-        for (int i = 0; i < arr.length; i++) {// add elements to heap
-            add(arr[i]);
+        data = new Vector<>(arr.length); // conocemos el tamaño final
+        for (int i = 0; i < arr.length; i++) {
+            add(arr[i]); // agrega los elementos al montículo
         }
     }
 
     /**
+     * Devuelve el índice del padre del elemento en el índice dado.
      *
-     * @param i
-     * @return
+     * @param i el índice del elemento cuyo padre se desea encontrar.
+     * @return el índice del padre del elemento en el índice dado.
      */
     protected static int parent(int i) {
         return (i - 1) / 2;
     }
+    /////
 
     /**
      *
@@ -85,31 +98,32 @@ public class VectorHeap<E extends Comparable<E>> implements Priority<E> {
         percolateUp(data.size() - 1);
     }
 
-    /**
-     *
-     * @param root
-     */
+        /**
+
+    Desciende el valor que se encuentra en la raíz de este VectorHeap, a su posición correspondiente dentro del heap.
+    @param root el índice de la raíz del heap.
+    */
     protected void pushDownRoot(int root) {
         int heapSize = data.size();
         E value = data.get(root);
         while (root < heapSize) {
-            int childpos = left(root);
-            if (childpos < heapSize) {
-                if ((right(root) < heapSize) && ((data.get(childpos + 1)).compareTo(data.get(childpos)) < 0)) {
-                    childpos++;
-                }
-                // Assert: childpos indexes smaller of two children
-                if ((data.get(childpos)).compareTo(value) < 0) {
-                    data.set(root, data.get(childpos));
-                    root = childpos; // keep moving down
-                } else { // found right location
-                    data.set(root, value);
-                    return;
-                }
-            } else { // at a leaf! insert and halt
-                data.set(root, value);
-                return;
-            }
+        int childpos = left(root);
+        if (childpos < heapSize) {
+        if ((right(root) < heapSize) && ((data.get(childpos + 1)).compareTo(data.get(childpos)) < 0)) {
+        childpos++;
+        }
+        // Afirmación: childpos indica el hijo más pequeño de los dos hijos.
+        if ((data.get(childpos)).compareTo(value) < 0) {
+        data.set(root, data.get(childpos));
+        root = childpos; // seguir descendiendo
+        } else { // se encontró la posición correcta
+        data.set(root, value);
+        return;
+        }
+        } else { // en una hoja! insertar y detenerse
+        data.set(root, value);
+        return;
+        }
         }
     }
 
